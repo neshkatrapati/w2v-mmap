76e39867e4607143fb47c214065578b2ea4813a3
Ganesh Katrapati <ganesh.katrapati@research.iiit.ac.in>
2017-04-14 22:08:04 +0530

    Added req.txt

--- index_w2vfile.py
+++ index_w2vfile.py
@@ -1,10 +1,7 @@
-from indexer import Indexer
 import sys
 
 source_file = sys.argv[1]
 index_file = source_file + '.idx'
 
-idxr = Indexer(source_file, index_file)
-idxr.make_index()
 
 sys.stdout.write("Done !!\n")
--- make_index.py
+++ make_index.py
@@ -1,21 +1,9 @@
 import sys
-import os
+from w2vmmap import * 
 
-file_name = sys.argv[1]
 
-def token_line_mapping(file_name):
-    key_file = file_name + '.kidx'
-    trie_file = file_name + '.trie'
+source_file = sys.argv[1]
+w2m = W2VMMap(source_file)
     
-    if os.path.exists(trie_file):
-        os.remove(trie_file)
-    #with open(file_name) as f ,open(key_file,'a') as t:
-    #    for index, line in enumerate(f):
-    #        token = line.split('\t')
-    #        t.write(token[0] + ';' + str(index) + "\n")
-    print "Done Writing to a File"
-    os.system('marisa-build {key_file} > {trie_file}'.format(**locals()))
-    print "Done Writing to Trie"
-    
-token_line_mapping(file_name)            
+w2m.make_index()            
             
